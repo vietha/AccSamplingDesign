cafb0b788b8afa6c63ff14ca84b8d87a99ba992f
Ha Truong <truongvietha87@gmail.com>
2025-04-22 20:53:39 +1200

    update summary to show USL/LSL

--- R/summary.R
+++ R/summary.R
@@ -22,18 +22,16 @@ summary.AttrPlan <- function(object, ...) {
   if(object$measurement_error > 0) {
     cat("Measurement Error:", object$measurement_error, "\n")
   }
+  cat("---------------------------------\n")
   invisible(object)
 }
 
 #' @export
 summary.VarPlan <- function(object, ...) {
   cat("Variable Acceptance Sampling Plan\n")
-  cat("--------------------------------\n")
+  cat("---------------------------------\n")
   cat("Distribution:", object$distribution, "\n")
-  if (!is.null(object$limit_type) && !is.null(object$spec_limit)) {
-    cat("Specification Limit:", object$limit_type, object$spec_limit, "\n")
-  }
-  
+
   cat("Sample Size (n):", object$sample_size, "\n")
   cat("Acceptance Limit (k):", round(object$k, 3), "\n")
   if(object$distribution == "normal") {
@@ -43,8 +41,15 @@ summary.VarPlan <- function(object, ...) {
   }
   cat("Producer's Risk (PR =", object$PR, ") at PRQ =", object$PRQ, "\n")
   cat("Consumer's Risk (CR =", object$CR, ") at CRQ =", object$CRQ, "\n")
+  if (!is.null(object$LSL)) {
+    cat("Lower Specification Limit:", object$LSL, "\n")
+  }
+  if (!is.null(object$USL)) {
+    cat("Uper Specification Limit:", object$USL, "\n")
+  }
   if(!is.null(object$measurement_error)) {
     cat("Measurement Error:", object$measurement_error, "\n")
   }
+  cat("---------------------------------\n")
   invisible(object)
 }
