fbc5b99a51c37bbd04572e761def09123d541705
Ha Truong <truongvietha87@gmail.com>
2025-04-27 07:09:27 +1200

    add poisson distribution

--- R/optAttrPlan.R
+++ R/optAttrPlan.R
@@ -10,10 +10,7 @@
 ## Changelogs:
 ## -----------------------------------------------------------------------------
 
-#' Attribute Acceptance Sampling Plan
-#' @export
-optAttrPlan <- function(PRQ, CRQ, alpha = 0.05, beta = 0.10, 
-                        measurement_error = 0) {
+find_plan_binomial <- function(PRQ, CRQ, alpha, beta, measurement_error = 0) {
   # Input validation
   if(PRQ >= CRQ) stop("PRQ must be < CRQ")
   if(alpha <= 0 || alpha >= 1) stop("alpha must be in (0,1)")
@@ -47,6 +44,74 @@ optAttrPlan <- function(PRQ, CRQ, alpha = 0.05, beta = 0.10,
     }
   }
   stop("No solution found within max_n = ", max_n)
+}
+
+
+find_plan_poisson <- function(PRQ, CRQ, alpha, beta, measurement_error = NULL) {
+  # Input validation
+  if (PRQ >= CRQ) stop("PRQ must be < CRQ")
+  if (alpha <= 0 || alpha >= 1) stop("alpha must be in (0,1)")
+  if (beta <= 0 || beta >= 1) stop("beta must be in (0,1)")
+  
+  max_n <- 1e5
+  for (n in 1:max_n) {
+    lambda_prq <- n * PRQ
+    lambda_crq <- n * CRQ
+    
+    # Find smallest c satisfying producer’s risk
+    c <- qpois(1 - alpha, lambda_prq, lower.tail = TRUE)
+    
+    pa_p <- ppois(c, lambda_prq)  # producer's risk at PRQ
+    pa_c <- ppois(c, lambda_crq)  # consumer's risk at CRQ
+    
+    if (pa_p >= 1 - alpha && pa_c <= beta) {
+      return(structure(
+        list(
+          n = n,
+          c = c,
+          PR = 1 - pa_p,
+          CR = pa_c,
+          PRQ = PRQ,
+          CRQ = CRQ,
+          sample_size = ceiling(n),
+          distribution = "poisson",
+          measurement_error = measurement_error
+        ),
+        class = "AttrPlan"
+      ))
+    }
+  }
+  
+  stop("No solution found within max_n = ", max_n)
+}
+
+#' Attribute Acceptance Sampling Plan
+#' @export
+optAttrPlan <- function(PRQ, CRQ, alpha = 0.05, beta = 0.10, 
+                        distribution = c("binomial", "poisson"),
+                        measurement_error = 0) {
+  distribution <- match.arg(distribution)
+  # Input validation
+  if(PRQ >= CRQ) stop("PRQ must be < CRQ")
+  if(alpha <= 0 || alpha >= 1) stop("alpha must be in (0,1)")
+  if(beta <= 0 || beta >= 1) stop("beta must be in (0,1)")
+  
+  # Init an optimal plan
+  attr_plan = NULL
+  
+  # ------------Binomial ----------
+  if (distribution == "binomial") {
+    attr_plan <- find_plan_binomial(PRQ = PRQ, CRQ = CRQ, 
+                                    alpha = alpha, beta = beta, 
+                                    measurement_error = measurement_error)
+  }
+  # ------------Poisson ------------
+  if (distribution == "poisson") {
+    attr_plan <- find_plan_poisson(PRQ = PRQ, CRQ = CRQ, 
+                                   alpha = alpha, beta = beta, 
+                                   measurement_error = measurement_error)
+  }
+  return(attr_plan)
 }
 
 
